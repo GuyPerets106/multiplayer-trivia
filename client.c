1371d882b8db1047670dba0277d52f16ab7f48a0
GuyPerets106 <guyperets106@gmail.com>
2024-06-29 14:28:36 +0300

    [AUTHENTICATION] Fixes

--- client.c
+++ client.c
@@ -8,15 +8,21 @@
 #include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <fcntl.h>
+#include <errno.h>
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER_IP "127.0.0.1"
+#define SERVER_IP "172.16.223.136"
 #define PORT 8080
-#define MULTICAST_IP "239.0.0.1"
+#define MULTICAST_IP "228.6.73.122"
 #define MULTICAST_PORT 12345
+#define AUTH_FAIL "Invalid authentication code"
+
+
 
 void receive_multicast() {
+    printf("GOT HERE\n");
     int sock;
     struct sockaddr_in addr;
     struct ip_mreq mreq;
@@ -24,7 +30,7 @@ void receive_multicast() {
 
     sock = socket(AF_INET, SOCK_DGRAM, 0);
     if (sock < 0) {
-        perror("socket");
+        perror("socket failure");
         exit(1);
     }
 
@@ -40,14 +46,14 @@ void receive_multicast() {
     addr.sin_port = htons(MULTICAST_PORT);
 
     if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-        perror("bind");
+        perror("bind failure");
         exit(1);
     }
 
     mreq.imr_multiaddr.s_addr = inet_addr(MULTICAST_IP);
     mreq.imr_interface.s_addr = htonl(INADDR_ANY);
     if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
-        perror("setsockopt");
+        perror("setsockopt failure");
         exit(1);
     }
 
@@ -65,32 +71,109 @@ void receive_multicast() {
 int main() {
     int sock = 0;
     struct sockaddr_in serv_addr;
-    char *hello = "Ready";
+    char *hello_msg = "Ready";
     char buffer[1024] = {0};
+    char server_ip[16];
+    int server_port;
+    int address_ok = 0;
 
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
+    while((sock = socket(AF_INET, SOCK_STREAM, 0)) >= 0) {
+        if (!address_ok) {
+            printf("Enter the IP address of the server: ");
+            scanf("%s", server_ip);
+            printf("Enter the port number of the server: ");
+            scanf("%d", &server_port);
+        }
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(server_port);
+        if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+            printf("\n**Invalid address**\n");
+            close(sock);
+            fflush(stdin);
+            continue;
+        }
+        else {
+            address_ok = 1;
+            fflush(stdin);
+        }
+
+        // Set the socket to non-blocking mode for connect only
+        int flags = fcntl(sock, F_GETFL, 0);
+        fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+
+        if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            if (errno == EINPROGRESS) {
+                struct timeval tv;
+                tv.tv_sec = 1; // Change if needed
+                tv.tv_usec = 0;
+                fd_set fdset;
+                FD_ZERO(&fdset);
+                FD_SET(sock, &fdset);
+                int ret = select(sock + 1, NULL, &fdset, NULL, &tv); 
+                if (ret < 0 && errno != EINTR) { 
+                    printf("Error in select function, Try again...\n");
+                    address_ok = 0;
+                    fflush(stdin);
+                    close(sock);
+                    continue;
+                }
+                else if (ret == 0) { // Timeout
+                    printf("Wrong IP or Port, Try again...\n");
+                    address_ok = 0;
+                    fflush(stdin);
+                    close(sock);
+                    continue;
+                }
+                else {
+                    int so_error;
+                    socklen_t len = sizeof(so_error);
+                    getsockopt(sock, SOL_SOCKET, SO_ERROR, &so_error, &len);
+                    if (so_error) {
+                        printf("Error connecting to server, Try again...\n");
+                        address_ok = 0;
+                        fflush(stdin);
+                        close(sock);
+                        continue;
+                    }
+                }
+            } else {
+                printf("Error connecting to server\n");
+                address_ok = 0;
+                fflush(stdin);
+                close(sock);
+                continue;
+            }
+        }
+        address_ok = 1;
+        // Set the socket back to blocking mode
+        flags = fcntl(sock, F_GETFL, 0);
+        fcntl(sock, F_SETFL, flags & ~O_NONBLOCK);
+        break;
+    }
+    char auth_buffer[1024];
+    int auth_success = 0;
+    while(!auth_success){
+        memset(auth_buffer, 0, sizeof(auth_buffer));
+        printf("Enter the authentication code: ");
+        scanf("%s", auth_buffer);
+        send(sock, auth_buffer, strlen(auth_buffer), 0);
+        int bytes_received = recv(sock, auth_buffer, sizeof(auth_buffer), 0);
+        auth_buffer[bytes_received] = '\0';
+        if (strncmp(auth_buffer, AUTH_FAIL, 27) == 0) {
+            printf("Invalid authentication code. Try again...\n");
+        }
+        else {
+            printf("Authentication successful\n");
+            auth_success = 1;
+        }
     }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-
-    send(sock, hello, strlen(hello), 0);
+    fflush(stdin);
+    send(sock, hello_msg, strlen(hello_msg), 0);
     printf("Ready message sent\n");
 
     pthread_t multicast_thread;
-    pthread_create(&multicast_thread, NULL, (void *)receive_multicast, NULL);
+    //pthread_create(&multicast_thread, NULL, (void *)receive_multicast, NULL);
 
     while (1) {
         printf("Enter your answer: ");
--- server.c
+++ server.c
@@ -10,24 +10,90 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <time.h>
 
 #define PORT 8080
-#define MULTICAST_IP "239.0.0.1"
+#define MULTICAST_IP "228.6.73.122"
 #define MULTICAST_PORT 12345
 #define MAX_CLIENTS 100
+#define START_GAME_TIMEOUT 30
 
 typedef struct {
     int socket;
     struct sockaddr_in address;
+    int authenticated;
 } Client;
 
 Client clients[MAX_CLIENTS];
 int client_count = 0;
 pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER; 
+char* auth_code;
+
+char* generate_random_code() { // Generates a 6 characters code (a-z/A-Z/0-9)
+    srand(time(NULL));
+    char* code = (char*)malloc(7);
+    for (int i = 0; i < 6; i++)
+    {
+        int type = rand() % 3;
+        switch (type)
+        {
+        case 0:
+            code[i] = 'a' + rand() % 26;
+            break;
+        case 1:
+            code[i] = 'A' + rand() % 26;
+            break;
+        case 2:
+            code[i] = '0' + rand() % 10;
+            break;
+        }
+    }
+    code[6] = '\0';
+    return code;
+}
+
+
+void* authenticate_client(void* arg) {
+    int* fds = (int*)arg;
+    int server_fd = fds[0];
+    int socket = fds[1];
+
+    int flags = fcntl(socket, F_GETFL, 0);
+    fcntl(socket, F_SETFL, flags & ~O_NONBLOCK);
+    char auth_buffer[1024];
+    while(1){
+        memset(auth_buffer, 0, sizeof(auth_buffer));
+        int ret = recv(socket, auth_buffer, sizeof(auth_buffer), 0);
+        if (ret == 0) break; // Closed socket
+        else if (ret < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                usleep(100000);  // Sleep for 100 milliseconds
+                continue;
+            }
+            else {
+                perror("Error receiving authentication code");
+                break;
+            }
+        }
+        printf("Received authentication code: %s\n", auth_buffer);
+        if (strcmp(auth_buffer, auth_code) != 0) {
+            send(socket, "Invalid authentication code", 27, 0);
+            continue;
+        }
+        else {
+            send(socket, "Authentication successful", 26, 0);
+            break;
+        }
+    }
+    free(arg);
+    return NULL;
+}
 
 void *new_client_handler(void *arg) {
     Client client = *(Client *)arg;
-    char buffer[1024]; // ?
+    char buffer[1024];
 
     // Receive client's readiness for the quiz
     recv(client.socket, buffer, sizeof(buffer), 0);
@@ -65,6 +131,7 @@ int main() {
     struct sockaddr_in address;
     int addrlen = sizeof(address);
     pthread_t thread_id;
+    auth_code = generate_random_code();
 
     // Create TCP socket
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
@@ -86,21 +153,83 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    printf("Server started. Waiting for connections...\n");
+    printf("Server started with authentication code %s. Waiting for connections...\n", auth_code);
+    time_t start_time = time(NULL);
+    while (1) {
+        // Get the current time
+        time_t current_time = time(NULL);
 
-    while ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) >= 0) {
+        // Calculate the elapsed time
+        double elapsed_time = difftime(current_time, start_time);
+
+        // Check if the elapsed time is greater than the allowed time frame
+        if (elapsed_time > START_GAME_TIMEOUT) {
+            printf("Time frame of %d seconds has expired. No more connections accepted.\n", START_GAME_TIMEOUT);
+            break;
+        }
+
+        // Set the socket to non-blocking mode
+        int flags = fcntl(server_fd, F_GETFL, 0);
+        fcntl(server_fd, F_SETFL, flags | O_NONBLOCK);
+
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
+        if (new_socket < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN) {
+                // No connection yet, sleep for a short time and try again
+                usleep(100000);  // Sleep for 100 milliseconds
+                continue;
+            } else {
+                perror("accept");
+                break;
+            }
+        }
+        printf("New request received\n");
+        int* fds = malloc(2 * sizeof(int));
+        if (fds == NULL) {
+            perror("malloc");
+            close(new_socket);
+            continue;
+        }
+
+        // Initialize the arguments
+        fds[0] = server_fd;
+        fds[1] = new_socket;
+
         pthread_mutex_lock(&client_mutex);
+        // Change client to authenticated = 0
+        clients[client_count].authenticated = 0;
+        pthread_mutex_unlock(&client_mutex);
+        
+        // Create a new thread to handle the connection
+        if (pthread_create(&thread_id, NULL, authenticate_client, (void*)fds) != 0) {
+            perror("pthread_create");
+            close(new_socket);
+            free(fds);
+            continue;
+        }
+        pthread_detach(thread_id);
+        
+        pthread_mutex_lock(&client_mutex);
+        if (clients[client_count].authenticated == 0) {
+            close(new_socket);
+            pthread_mutex_unlock(&client_mutex);
+            continue;
+        }
+        else {
         clients[client_count].socket = new_socket;
         clients[client_count].address = address;
         client_count++;
         pthread_mutex_unlock(&client_mutex);
+        printf("New connection accepted: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
+        }
 
         pthread_create(&thread_id, NULL, new_client_handler, &clients[client_count - 1]);
         pthread_detach(thread_id);
 
         printf("New client connected: %s:%d\n", inet_ntoa(address.sin_addr), ntohs(address.sin_port));
     }
-
+    printf("Game started. Broadcasting questions...\n");
+    while(1);
     close(server_fd);
     return 0;
 }
