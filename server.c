dd032f4341c49c2882f4791f07afbe096b223f62
GuyPerets106 <guyperets106@gmail.com>
2024-07-06 21:12:20 +0300

    printed answer

--- server.c
+++ server.c
@@ -413,6 +413,8 @@ void handle_client_answer(int client_sock, char* client_answer) {
     pthread_mutex_lock(&client_mutex);
     for (int i = 0; i < client_count; i++) {
         if (clients[i].socket == client_sock) {
+            printf("Received answer from client: %s\n", client_answer);
+            printf("Correct answer: %s\n", questions[curr_question_index].answer); // ! CHANGE
             if (strcmp(client_answer, questions[curr_question_index].answer) == 0) { // ! CHANGE
                 // Compute elapsed time in seconds
                 int elapsed_time = (int)difftime(current_time, curr_question_start_time);
