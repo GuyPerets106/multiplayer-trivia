6f4fb0b6d7ed7d3b12b837ab97bc793ab3727449
GuyPerets106 <guyperets106@gmail.com>
2024-07-13 15:11:20 +0300

    fix

--- client.c
+++ client.c
@@ -342,6 +342,7 @@ void* handle_message(void* args) {
             curr_question_thread = pthread_self(); // ! Consider Mutex
             answer_question();
             send_message(client_socket, ANSWER, curr_answer);
+            curr_answer[0] = '\0';
             break;
         case ANSWER: // ! Receive Unicast When Timeout
             pthread_cancel(curr_question_thread);
